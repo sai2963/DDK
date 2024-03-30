958a722abd4e02da0107394453deef352991a64f
sai2963 <venkatsai0629@gmail.com>
2024-03-30 21:10:23 +0530

    A new Project Started

---
+++ app.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const path=require('path')
+const autroutes=require("./routes/auth.route")
+const app=express();
+
+app.use(autroutes);
+app.use(express.static('views/styles'));
+app.use(express.static('images'));
+app.set('view engine' ,'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    res.redirect('/signup')
+})
+
+app.listen(3000 ,() => {
+    console.log("Server Running")
+})
---
+++ controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+function getSignup(req,res){
+    res.render('customer/auth/signup')
+}
+function getLogin(req,res){
+    
+}
+
+module.exports={
+    getSignup:getSignup,
+    getLogin:getLogin,
+}
