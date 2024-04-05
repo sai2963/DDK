c378b22c9f110ec1b7c741f0362126b1cb96d3e5
sai2963 <venkatsai0629@gmail.com>
2024-04-05 23:29:09 +0530

    auth.controllers updated

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -11,14 +11,7 @@ function getLogin(req, res) {
   res.render("customer/auth/login");
 }
 async function signup(req, res) {
-//   const userdata = req.body;
-//   console.log(userdata);
-//   const useremail = userdata.email;
-//   const userpassword = userdata.password;
-//   const userfullname = userdata.fullname;
-//   const userstreet = userdata.street;
-//   const userpostal = userdata.postalcode;
-//   const usercity = userdata.city;
+
   const user = new User(
     req.body.email,
     req.body.password,
